8165bc5aac20594c9c8ce178065908ebb1818aee
Junaid <mjks4545@gmail.com>
2016-06-07 11:37:56 +0500

    comment
    comment

--- conn.php
+++ conn.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
+$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbname   = getenv('OPENSHIFT_APP_NAME');
 
 try {
   //database connection
-     $conn = new PDO("mysql:host=$servername;dbname=final_project", $username, $password);
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
      // set the PDO error mode to exception
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     }
